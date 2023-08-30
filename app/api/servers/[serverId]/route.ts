27e7505e6f77aae962fdcb92d25d185f9ec35749
IvanMilicevic <ivanmilicevicmeda@gmail.com>
2023-08-30 18:43:45 +0200

    Server Settings

---
+++ app/api/servers/[serverId]/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } },
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!params.serverId) {
+      return new NextResponse("Server ID is required", { status: 400 });
+    }
+
+    const { name, imageUrl } = await req.json();
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+      data: {
+        name,
+        imageUrl,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (err) {
+    console.log("[SERVER_ID_PATCH]", err);
+
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
