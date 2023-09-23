90ce484b88034558aced100d5a082361070ff73e
IvanMilicevic <ivanmilicevicmeda@gmail.com>
2023-09-23 09:08:12 +0200

    Channel Creation

---
+++ app/api/channels/route.ts
@@ -0,0 +1,54 @@
+import { NextResponse } from "next/server";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { MemberRole } from "@prisma/client";
+
+export async function POST(req: Request, res: Response) {
+  try {
+    const profile = await currentProfile();
+    const { name, type } = await req.json();
+
+    const { searchParams } = new URL(req.url);
+    const serverId = searchParams.get("serverId");
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    if (!serverId) {
+      return new NextResponse("Server ID missing", { status: 400 });
+    }
+    if (name === "general" || name === "General") {
+      return new NextResponse("Cannot create a channel with that name", {
+        status: 400,
+      });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+            },
+          },
+        },
+      },
+      data: {
+        channels: {
+          create: {
+            profileId: profile.id,
+            name,
+            type,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (err) {
+    console.log(err);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
